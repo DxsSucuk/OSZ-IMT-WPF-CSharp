aafbf3727a091dc14bf594397a985635e7aefaa9
DxsSucuk <53257574+DxsSucuk@users.noreply.github.com>
2022-09-21 15:46:38 +0200

    Smash 2

---
+++ Schiffeversenken/Farben.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Schiffeversenken
+{
+    public class Farben
+    {
+        public static void start()
+        {
+            Console.ResetColor();
+            int wert;
+            Console.WriteLine("Bitte geben einen Wert zwischen 1 und 5 an.");
+            wert = Convert.ToInt32(Console.ReadLine());
+
+            switch (wert)
+            {
+                case 1:
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.Write("  ");
+                    break;
+                }
+
+                case 2:
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Write("  ");
+                    Console.Write(" ");
+                    break;
+                }
+
+                case 3:
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.Write("  "); 
+                    Console.Write("  "); 
+                    Console.Write("  "); 
+                    break;
+                }
+
+                case 4:
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Write("  "); 
+                    Console.Write("  "); 
+                    Console.Write("  "); 
+                    Console.Write("  "); 
+                    break;
+                }
+
+                case 5:
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write("  "); 
+                    Console.Write("  "); 
+                    Console.Write("  "); 
+                    Console.Write("  "); 
+                    Console.Write("  "); 
+                    break;
+                }
+
+                default:
+                {
+                    Console.WriteLine("Invalid input");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Schiffeversenken/Gelaende.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Schiffeversenken
+{
+    public class Gelaende
+    {
+        public static void start()
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("  ");
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.Write("  ");
+            
+            Console.WriteLine();
+            
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("  ");
+            Console.Write("  ");
+            Console.Write("  ");
+            
+            Console.WriteLine();
+            
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("  ");
+            
+            Console.WriteLine();
+            
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("  ");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("  ");
+            
+            Console.WriteLine();
+            Farben.start();
+        }
+    }
+}
